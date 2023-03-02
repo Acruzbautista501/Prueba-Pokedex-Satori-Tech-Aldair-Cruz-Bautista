ecd866da0e031802066d9fb23a45697d9ea915ea
Aldair Cruz Bautista <112189881+Acruzbautista501@users.noreply.github.com>
2023-03-02 11:16:51 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,96 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonImagesvg = document.querySelector('.pokemon_image_svg');
+const pokebadge = document.querySelector('.pokebadge');
+
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonView = document.querySelector('.btn-mp')
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = 0;
+
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+  if (APIResponse.status === 200) {
+    const data = await APIResponse.json();
+    return data;
+  }
+}
+
+const renderPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = 'Cargando...';
+  pokemonNumber.innerHTML = '';
+
+  const data = await fetchPokemon(pokemon);
+
+  if (data) {
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    pokemonImagesvg.src = data['sprites']['other']['official-artwork']['front_default'];
+    pokebadge.src = data['sprites']['other']['dream_world']['front_default'];
+    
+    //data['sprites']['other']['official-artwork']['front_default'];
+
+
+    
+    input.value = '';
+    searchPokemon = data.id;
+  } else {
+    pokemonImage.style.display = 'none';
+    pokemonName.innerHTML = 'Selecciona una opción';
+    pokemonNumber.innerHTML = '';
+  }
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  renderPokemon(input.value.toLowerCase());
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonNext.addEventListener('click', () => {
+  if (searchPokemon => 1) {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+buttonView.addEventListener('click', () => {
+  if (searchPokemon => 1) {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  }
+});
+
+
+renderPokemon(searchPokemon);
+
+    //image slider autoplay
+    var playSlider;
+
+    var repeater = () => {
+      playSlider = setInterval(function(){
+
+        if (searchPokemon => 1) {
+          searchPokemon += 1;
+          renderPokemon(searchPokemon);
+        }
+      }, 30000);
+    }
+    repeater();
+
+
